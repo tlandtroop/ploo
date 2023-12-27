23bb6a1cbad9e9f542461717af85984939624b2a
Tyler Landtroop <tlandtroop@ufl.edu>
2023-12-27 11:40:00 -0600

    about card

---
+++ actions/user.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { User } from "@prisma/client";
+
+import { getSelf } from "@/lib/auth-service";
+import { db } from "@/lib/db";
+
+export const updateUser = async (values: Partial<User>) => {
+  const self = await getSelf();
+
+  const validData = {
+    bio: values.bio,
+  };
+
+  const user = await db.user.update({
+    where: {
+      id: self.id,
+    },
+    data: { ...validData },
+  });
+
+  revalidatePath(`/${self.username}`);
+  revalidatePath(`/u/${self.username}`);
+
+  return user;
+};
