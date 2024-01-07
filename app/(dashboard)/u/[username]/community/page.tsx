f77ea1ee6576a49d85c5891eb9fa9a6ab9dc0077
Tyler Landtroop <tlandtroop@ufl.edu>
2024-01-06 23:03:01 -0500

    community page

---
+++ app/(dashboard)/u/[username]/community/page.tsx
@@ -0,0 +1,29 @@
+import { format } from "date-fns";
+
+import { getBlockedUser } from "@/lib/block-service";
+
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+
+const CommunityPage = async () => {
+  const blockedUsers = await getBlockedUser();
+
+  const formattedData = blockedUsers.map((block) => ({
+    ...block,
+    userId: block.blocked.id,
+    imageUrl: block.blocked.imageUrl,
+    username: block.blocked.username,
+    createdAt: format(new Date(block.blocked.createdAt), "MM/dd/yyyy"),
+  }));
+
+  return (
+    <div className="p-6">
+      <div className="mb-4">
+        <h1 className="text-2xl font-bold">Community Settings</h1>
+      </div>
+      <DataTable columns={columns} data={formattedData} />
+    </div>
+  );
+};
+
+export default CommunityPage;
